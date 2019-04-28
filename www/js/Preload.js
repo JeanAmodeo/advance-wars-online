dbb28c2d88fabe018ffc639e1e3c3c72de759584
JeanAmodeo <34283042+JeanAmodeo@users.noreply.github.com>
2019-04-28 23:13:36 +0100

    Revert "Merge branch 'range'"
    This reverts commit bf3eb045503ddfa2ca55e6f45acb78c6f61c7b2f, reversing
    changes made to cf030b08a0be205c26731454f41f4f95e9f47ef1.

--- www/js/Preload.js
+++ www/js/Preload.js
@@ -7,7 +7,7 @@ Strategy.Preload.prototype = {
 		this.load.spritesheet('units', 'assets/images/units.png', 16, 16, 3);
 		this.load.image('healthbar', 'assets/images/healthbar.png');
 	},
-
+	
 	create: function() {
 		this.game.state.start('Game');
 
